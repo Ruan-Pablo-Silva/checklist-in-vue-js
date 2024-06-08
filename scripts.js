0cb3a354d2451c45d688ebcded59c429a05ee8a4
Ruan-Pablo-Silva <silvaruanpablo8@gmail.com>
2024-06-08 11:32:57 -0300

    fixed bug where adding new data would mmodify old ones, added erase all possibility

--- scripts.js
+++ scripts.js
@@ -7,19 +7,23 @@ const objectLists = {
   data() {
     return {
       listData: window.todoObjects,
-      newToDo: {
-        toDoTxt: null
-      },
+      newToDo: { toDoTxt: null}, //  binding with v-model
     };
   },
   methods:{
-    addToDo: function() {
-      todoObjects.push(this.newToDo)
-    },
-
     addNewItemOnToDoList: function() {
-      if(this.newToDo.toDoTxt !== null){
+      if(this.newToDo.toDoTxt !== null && this.newToDo.toDoTxt.trim().length > 0){
         this.listData.push(this.newToDo)
+        console.log(this.listData)
+        this.newToDo = {toDoTxt: null} //passing a new object 
+      }
+    },
+    
+    eraseAllData: function(){
+      if(this.listData.length > 0){
+        this.listData = {}
+        console.log(this.listData)
+
       }
 
     }
